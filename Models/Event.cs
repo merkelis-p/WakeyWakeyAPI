f99c5462339a1112ff7b6aa60ead08b42b116988
merkelis-p <patrikas.merkelis@gmail.com>
2023-11-15 00:55:22 +0200

    merging conflicts

--- Models/Event.cs
+++ Models/Event.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-#nullable disable
 
 namespace WakeyWakeyAPI.Models
 {
@@ -12,22 +11,26 @@ namespace WakeyWakeyAPI.Models
         public Event()
         {
             Reminders = new HashSet<Reminder>();
+            Name = string.Empty;
         }
 
         [Key]
         public int Id { get; set; }
+        [Required]
         public DateTime StartDate { get; set; }
+        [Required]
         public DateTime EndDate { get; set; }
         public int? Recurring { get; set; }
         [Required]
         public string Name { get; set; }
-        public string Description { get; set; }
-        public string Location { get; set; }
+        public string? Description { get; set; }
+        public string? Location { get; set; }
 
+        [Required]
         [ForeignKey("User")]
         public int UserId { get; set; }
-        public virtual User User { get; set; }
+        public virtual User? User { get; set; }
 
-        public virtual ICollection<Reminder> Reminders { get; set; }
+        public virtual ICollection<Reminder>? Reminders { get; set; }
     }
 }
