3875aae986269e0dc7fcec0a4f45068abbfa46c0
merkelis-p <patrikas.merkelis@gmail.com>
2023-11-15 03:17:16 +0200

    test

--- Models/Task.cs
+++ Models/Task.cs
@@ -43,8 +43,6 @@
             [Required]
             public int Status { get; set; }
             
-            // Navigation property for the parent task
-            public Task? ParentTask { get; set; }
 
             // Collection of subtasks
             public ICollection<Task>? SubTasks { get; set; }
