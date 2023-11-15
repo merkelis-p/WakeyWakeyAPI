bf98de9f931e471a689707cac62a32970e856c2d
merkelis-p <patrikas.merkelis@gmail.com>
2023-11-15 03:02:04 +0200

    some fixes

--- Repositories/SubjectRepository.cs
+++ Repositories/SubjectRepository.cs
@@ -24,6 +24,8 @@ namespace WakeyWakeyAPI.Repositories
         
         
         
+        
+        
       
     }
 }
--- Repositories/TaskRepository.cs
+++ Repositories/TaskRepository.cs
@@ -138,6 +138,10 @@ namespace WakeyWakeyAPI.Repositories
         }
         
         
+        
+        
+        
+        
 
         
     }
